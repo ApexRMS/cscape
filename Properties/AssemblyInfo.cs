65a3fe323956460165cab3f49114c7d2197100c8
alex <alexe_webmail@yahoo.com>
2019-01-17 18:51:05 -0800

    Refactoring of type names for Modules to Packages conversion.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SyncroSim.Circuitscape")]
-[assembly: AssemblyDescription("Circuitscape Module")]
+[assembly: AssemblyDescription("Circuitscape Package")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ApexRMS")]
 [assembly: AssemblyProduct("SyncroSim.Circuitscape")]
